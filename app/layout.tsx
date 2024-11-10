43eea9b6aec2aeeaaeb7d6dadb8ec6c5ad9fbd05
Jordi Petit <jordi.petit@gmail.com>
2024-11-10 17:46:57 +0100

    Create project

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from 'next'
+import localFont from 'next/font/local'
+import './globals.css'
+
+const geistSans = localFont({
+    src: './fonts/GeistVF.woff',
+    variable: '--font-geist-sans',
+    weight: '100 900',
+})
+const geistMono = localFont({
+    src: './fonts/GeistMonoVF.woff',
+    variable: '--font-geist-mono',
+    weight: '100 900',
+})
+
+export const metadata: Metadata = {
+    title: 'Jutge.org API Doc',
+    description: 'Jutge.org API v2 documentation',
+}
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode
+}>) {
+    return (
+        <html lang="en">
+            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+                {children}
+            </body>
+        </html>
+    )
+}
