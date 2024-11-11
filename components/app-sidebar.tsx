d798f46d9b8a3b1d624a6ab4417581913f13b38b
Jordi Petit <jordi.petit@gmail.com>
2024-11-11 10:47:59 +0100

    Improve directory

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,154 +1,221 @@
-'use client'
-
-import * as React from 'react'
-import {
-    BookOpen,
-    Package,
-    Gavel,
-    Type,
-    SquareFunction,
-    Map,
-    PieChart,
-    Send,
-    Settings2,
-    SquareTerminal,
-    LifeBuoy,
-    SquareChevronRight,
-} from 'lucide-react'
-import Image from 'next/image'
-import { NavMain } from '@/components/nav-main'
-import { NavSecondary } from '@/components/nav-secondary'
-import { NavUser } from '@/components/nav-user'
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/components/ui/collapsible'
 import {
     Sidebar,
     SidebarContent,
     SidebarFooter,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
     SidebarHeader,
     SidebarMenu,
     SidebarMenuButton,
     SidebarMenuItem,
+    SidebarMenuSub,
 } from '@/components/ui/sidebar'
-import { isAbsolute } from 'path'
+import { ChevronRight, Package, SquareChevronRight, SquareFunction, Type } from 'lucide-react'
+import Image from 'next/image'
+import * as React from 'react'
+import { NavUser } from './nav-user'
 
-type Props = React.ComponentProps<typeof Sidebar> & {
-    dir: any
+type Item = {
+    name: string
+    url: string
+    type: string
+    isActive?: boolean
+    items?: Item[]
 }
 
-function models(mod: any) {
+type Items = Item[]
+
+type Module = any
+
+function models(mod: Module): Items {
     return mod.models.map((model: any) => {
         return {
-            title: model[0],
+            name: model[0],
             url: '#',
-            icon: Type,
+            type: 'model',
             isActive: false,
         }
     })
 }
 
-function endpoints(mod: any) {
+function endpoints(mod: Module): Items {
     return mod.endpoints.map((endpoint: any) => {
         return {
-            title: endpoint.name,
+            name: endpoint.name,
             url: '#',
-            icon: SquareFunction,
+            type: 'endpoint',
             isActive: false,
         }
     })
 }
 
-function modules(mod: any) {
+function modules(mod: Module): Items {
     return mod.modules.map((mod: any) => {
         return {
-            title: mod.name,
+            name: mod.name,
             url: '#',
-            icon: Package,
+            type: 'module',
             isActive: false,
             items: models(mod).concat(endpoints(mod)).concat(modules(mod)),
         }
     })
 }
 
-export function AppSidebar({ ...props }: Props) {
+type AppSidebarProps = React.ComponentProps<typeof Sidebar> & {
+    dir: Module
+}
+
+export function AppSidebar({ ...props }: AppSidebarProps) {
+    const clients: Items = [
+        {
+            name: 'TypeScript',
+            type: 'client',
+            url: '#',
+            isActive: false,
+        },
+        {
+            name: 'Python',
+            type: 'client',
+            url: '#',
+            isActive: false,
+        },
+        {
+            name: 'Shell',
+            type: 'client',
+            url: '#',
+            isActive: false,
+        },
+    ]
+
+    const tree = modules(props.dir)
+
     const user = {
         name: 'guest',
         email: 'guest@example.com',
         avatar: '/avatars/shadcn.jpg',
     }
 
-    const clientsItems: any[] = [
-        {
-            title: 'TypeScript',
-            url: '#',
-            icon: SquareChevronRight,
-            isActive: false,
-        },
-        {
-            title: 'Python',
-            url: '#',
-            icon: SquareChevronRight,
-            isActive: false,
-        },
-        {
-            title: 'Shell',
-            url: '#',
-            icon: SquareChevronRight,
-            isActive: false,
-        },
-    ]
-
-    const data = {
-        user,
-        dirMenu: {
-            title: 'API Directory',
-            items: modules(props.dir),
-        },
-        clientsMenu: {
-            title: 'API Clients',
-            items: clientsItems,
-        },
-        navSecondary: [
-            {
-                title: 'Support',
-                url: '#',
-                icon: LifeBuoy,
-            },
-            {
-                title: 'Feedback',
-                url: '#',
-                icon: Send,
-            },
-        ],
-    }
-
     return (
-        <Sidebar variant="inset" {...props}>
-            <SidebarHeader>
-                <SidebarMenu>
-                    <SidebarMenuItem>
-                        <SidebarMenuButton size="lg" asChild>
-                            <a href="#">
-                                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                                    <Image src="/jutge.png" width={64} height={64} alt="" />
-                                </div>
-                                <div className="grid flex-1 text-left text-sm leading-tight">
-                                    <span className="truncate font-semibold">
-                                        API Documentation
-                                    </span>
-                                    <span className="truncate text-xs">Jutge.org</span>
-                                </div>
-                            </a>
-                        </SidebarMenuButton>
-                    </SidebarMenuItem>
-                </SidebarMenu>
-            </SidebarHeader>
+        <Sidebar {...props}>
+            <Header />
+
             <SidebarContent>
-                <NavMain menu={data.clientsMenu} />
-                <NavMain menu={data.dirMenu} />
-                <NavSecondary items={data.navSecondary} className="mt-auto" />
+                <Clients clients={clients} />
+                <Directory tree={tree} />
             </SidebarContent>
+
             <SidebarFooter>
-                <NavUser user={data.user} />
+                <NavUser user={user} />
             </SidebarFooter>
         </Sidebar>
     )
+}
+
+function Header() {
+    return (
+        <SidebarHeader>
+            <SidebarMenu>
+                <SidebarMenuItem>
+                    <SidebarMenuButton size="lg" asChild>
+                        <a href="#">
+                            <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                                <Image src="/jutge.png" width={64} height={64} alt="" />
+                            </div>
+                            <div className="grid flex-1 text-left text-sm leading-tight">
+                                <span className="truncate font-semibold">API Documentation</span>
+                                <span className="truncate text-xs">Jutge.org</span>
+                            </div>
+                        </a>
+                    </SidebarMenuButton>
+                </SidebarMenuItem>
+            </SidebarMenu>
+        </SidebarHeader>
+    )
+}
+
+function Clients({ clients }: { clients: Items }) {
+    return (
+        <SidebarGroup>
+            <SidebarGroupLabel>API Clients</SidebarGroupLabel>
+            <SidebarGroupContent>
+                <SidebarMenu>
+                    {clients.map((item, index) => (
+                        <SidebarMenuItem key={index}>
+                            <SidebarMenuButton>
+                                <SquareChevronRight />
+                                {item.name}
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    ))}
+                </SidebarMenu>
+            </SidebarGroupContent>
+        </SidebarGroup>
+    )
+}
+
+function Directory({ tree: treeDir }: { tree: Items }) {
+    return (
+        <SidebarGroup>
+            <SidebarGroupLabel>API Directory</SidebarGroupLabel>
+            <SidebarGroupContent>
+                <SidebarMenu>
+                    {treeDir.map((item, index) => (
+                        <Tree key={index} item={item} />
+                    ))}
+                </SidebarMenu>
+            </SidebarGroupContent>
+        </SidebarGroup>
+    )
+}
+
+function Tree({ item }: { item: Item }) {
+    if (item.type === 'model') {
+        return (
+            <SidebarMenuButton isActive={false} className="data-[active=true]:bg-transparent">
+                <Type />
+                {item.name}
+            </SidebarMenuButton>
+        )
+    }
+
+    if (item.type === 'endpoint') {
+        return (
+            <SidebarMenuButton isActive={false} className="data-[active=true]:bg-transparent">
+                <SquareFunction />
+                {item.name}
+            </SidebarMenuButton>
+        )
+    }
+
+    if (item.type === 'module') {
+        return (
+            <SidebarMenuItem>
+                <Collapsible
+                    className="group/collapsible [&[data-state=open]>button>svg:first-child]:rotate-90"
+                    defaultOpen={item.name === 'components' || item.name === 'ui'}
+                >
+                    <CollapsibleTrigger asChild>
+                        <SidebarMenuButton>
+                            <ChevronRight className="transition-transform" />
+                            <Package />
+                            {item.name}
+                        </SidebarMenuButton>
+                    </CollapsibleTrigger>
+                    {item.items ? (
+                        <CollapsibleContent>
+                            <SidebarMenuSub>
+                                {item.items.map((subItem: any, index: number) => (
+                                    <Tree key={index} item={subItem} />
+                                ))}
+                            </SidebarMenuSub>
+                        </CollapsibleContent>
+                    ) : null}
+                </Collapsible>
+            </SidebarMenuItem>
+        )
+    }
+
+    console.log('Unknown item type:', item.type)
 }
