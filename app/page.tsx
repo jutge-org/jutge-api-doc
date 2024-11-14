224b41429bf171499dc37688d6c8a066e436e2df
Pau Fernández <pau.fernandez@upc.edu>
2024-11-14 13:43:49 +0100

    Incomplete, but much better type representation

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import YAML from 'yaml'
 import { AppSidebar } from '@/components/app-sidebar'
 import {
     Breadcrumb,
@@ -10,9 +9,12 @@ import {
 } from '@/components/ui/breadcrumb'
 import { Separator } from '@/components/ui/separator'
 import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
-import { Package, SquareFunction, Type } from 'lucide-react'
-import { jutgeApiAddress } from '@/lib/utilities'
 import { modules } from '@/lib/tree'
+import { jutgeApiAddress } from '@/lib/utilities'
+import { cn } from '@/lib/utils'
+import { Package, SquareFunction, Type } from 'lucide-react'
+import Link from 'next/link'
+import YAML from 'yaml'
 
 export default async function Page() {
     const response = await fetch(`${jutgeApiAddress()}/api/dir`)
@@ -65,14 +67,14 @@ function Main({ dir }: any) {
 function Module({ mod, path }: any) {
     const header =
         path.length === 0 ? null : (
-            <h1 className="py-4 flex gap-2 rounded-xlpl-4 font-semibold">
+            <h1 className="pt-1 pl-1 pb-4 flex gap-2 rounded-xl font-semibold">
                 <Package />
                 {path.join('.')}
             </h1>
         )
 
     const models = mod.models.map((model: any) => (
-        <Model key={model.name} model={model} path={path} />
+        <Model key={crypto.randomUUID()} model={model} path={path} />
     ))
 
     const endpoints = mod.endpoints.map((endpoint: any) => (
@@ -86,8 +88,10 @@ function Module({ mod, path }: any) {
     return (
         <div id={path}>
             {header}
-            <div className="pl-8">{models}</div>
-            <div className="pl-8">{endpoints}</div>
+            {models.length > 0 && <div className="px-8 pb-12 flex flex-col gap-8">{models}</div>}
+            {endpoints.length > 0 && (
+                <div className="px-8 pb-12 flex flex-col gap-8">{endpoints}</div>
+            )}
             {submodules}
         </div>
     )
@@ -96,29 +100,105 @@ function Module({ mod, path }: any) {
 function Model({ model, path }: any) {
     const [name, data] = model
     return (
-        <div className="p-4" id={`${path.join(".")}.${name}`}>
+        <div id={`${path.join('.')}.${name}`}>
             <h2 className="font-semibold flex gap-2">
-                <Type />
-                {name}
+                <Type className="w-5 h-5" />
+                <code>{name}</code>
             </h2>
-            <pre className="text-xs pl-2 pt-2">{YAML.stringify(data, null, 4)}</pre>
+            <TypeView input={data} path={path} />
         </div>
     )
 }
 
 function Endpoint({ endpoint, path }: any) {
     return (
-        <div className="p-4" id={`${path.join(".")}.${endpoint.name}`}>
-            <h2 className="font-semibold flex gap-2">
-                <SquareFunction />
-                {endpoint.name}
+        <div id={`${path.join('.')}.${endpoint.name}`}>
+            <h2 className="font-semibold flex flex-row gap-2 items-center">
+                <SquareFunction className="w-6 h-6" />
+                <code className="mt-1">{endpoint.name}</code>
+                <span className="font-normal">
+                    <span className="text-gray-400">&mdash;</span>
+                    <span className="ml-2 text-sm">{endpoint.summary}</span>
+                </span>
             </h2>
-            <p>{endpoint.summary}</p>
-            <p>{endpoint.description}</p>
-            <h3>Input</h3>
-            <pre className="text-xs pl-2 py-2">{YAML.stringify(endpoint.input, null, 4)}</pre>
-            <h3>Output</h3>
-            <pre className="text-xs pl-2 py-2">{YAML.stringify(endpoint.output, null, 4)}</pre>
+            {endpoint.description && (
+                <div className="pb-3 pl-8 text-sm max-w-[45em]">
+                    <p>{endpoint.description}</p>
+                </div>
+            )}
+            <div className="flex flex-col gap-1 mt-1 pl-8">
+                <TypeView name="input" input={endpoint.input} path={path} />
+                <TypeView name="output" input={endpoint.output} path={path} />
+            </div>
+        </div>
+    )
+}
+
+type TypeViewProps = {
+    name?: string
+    input: Record<string, any>
+    path: string[]
+}
+function TypeView({ name, input, path }: TypeViewProps) {
+    let body = <pre className="text-xs pl-2 pt-2">{YAML.stringify(input, null, 4)}</pre>
+
+    const _BasicType = ({ type }: { type: string }) => (
+        <div className="bg-gray-100 box-border border border-gray-200 rounded-full px-2 text-gray-800">
+            {type}
+        </div>
+    )
+
+    const _Reference = ({ name }: { name: string }) => (
+        <Link href={`#${path.join('.')}.${name}`}>
+            <code>{input.$ref}</code>
+        </Link>
+    )
+
+    const _AnyOf = ({ types }: { types: any[] }) => (
+        <div className="flex flex-row items-center gap-3">
+            <TypeView input={types[0]} path={path} />
+            {types.slice(1).map((type) => (
+                <>
+                    <div className="h-4 border"></div>
+                    <TypeView key={type.type} input={type} path={path} />
+                </>
+            ))}
+        </div>
+    )
+
+    const _Object = ({ properties }: { properties: Record<string, any> }) => (
+        <div className="border px-3 p-1 rounded">
+            <div className="flex flex-col gap-1">
+                {Object.entries(properties).map(([name, type]) => (
+                    <div key={name} className="flex flex-row gap-2 items-baseline">
+                        <code className="text-sm">{name}</code>
+                        <TypeView input={type} path={path} />
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+
+    if (input.$ref) {
+        body = <_Reference name={input.$ref} />
+    } else if (input.anyOf) {
+        body = <_AnyOf types={input.anyOf} />
+    } else if (input.type === 'object') {
+        if (input.properties) {
+            body = <_Object properties={input.properties} />
+        }
+    } else {
+        body = <_BasicType type={input.type} />
+    }
+    return (
+        <div
+            className={cn(
+                'flex flex-row gap-3 text-sm items-baseline',
+                input.type === 'object' && !name ? 'pl-8' : '',
+            )}
+        >
+            {name && <h4 className="min-w-[4em] text-gray-700">{name}</h4>}
+            {body}
         </div>
     )
 }
