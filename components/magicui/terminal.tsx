822c985ff7c0ab3612a053142116b7d783593093
Jordi Petit <jordi.petit@gmail.com>
2025-02-07 17:52:05 +0100

    Resize

--- components/magicui/terminal.tsx
+++ components/magicui/terminal.tsx
@@ -100,7 +100,7 @@ export const Terminal = ({ title, children, className }: TerminalProps) => {
                 className,
             )}
         >
-            <div className="flex flex-col gap-y-2 border-b border-border p-3">
+            <div className="flex flex-col gap-y-2 border-b border-border p-2">
                 <div className="flex flex-row items-center gap-x-2">
                     <div className="h-3 w-3 rounded-full bg-red-500"></div>
                     <div className="h-3 w-3 rounded-full bg-yellow-500"></div>
