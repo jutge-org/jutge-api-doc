3d45607be0595fc07c4b2e0e7d15235c8569b2b6
Pau Fernández <pau.fernandez@upc.edu>
2025-02-15 16:00:19 +0100

    /documentation is dynamic

--- app/documentation/page.tsx
+++ app/documentation/page.tsx
@@ -2,6 +2,8 @@ import Module from "@/components/documentation/module"
 import PageWidth from "@/components/page-width"
 import { getApiDir } from "@/lib/api-dir"
 
+export const dynamic = 'auto'
+
 export default async function Page() {
     const { models, root } = await getApiDir()
     return (
