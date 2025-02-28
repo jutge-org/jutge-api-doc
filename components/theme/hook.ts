5664220e8c9352a56619dfb3e60538029546c1f9
Pau Fernández <pau.fernandez@upc.edu>
2025-02-28 13:59:43 +0100

    Removed debug prints

--- components/theme/hook.ts
+++ components/theme/hook.ts
@@ -5,13 +5,11 @@ export function useTheme() {
     const { mode, setMode } = useContext(ThemeContext)
 
     const setAndSaveMode = (mode: "light" | "dark") => {
-        console.log("Setting mode", mode);
         if (mode === "light") {
             localStorage.removeItem("dark")
         } else {
             localStorage.setItem("dark", "true")
         }
-        console.log(localStorage.getItem("dark"))
         setMode(mode)
     }
 
