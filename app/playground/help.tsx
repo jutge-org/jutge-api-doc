6487d741cf64ac60b62f9a8b4a3fdd2d1fc697a7
Pau Fernández <pau.fernandez@upc.edu>
2025-03-08 17:05:18 +0100

    Polished help more

--- app/playground/help.tsx
+++ app/playground/help.tsx
@@ -1,49 +1,62 @@
-import { cn } from "@/lib/utils"
 import PlatformCtrlKbd from "@/components/platform-ctrl-kbd"
+import TextWidth from "@/components/text-width"
+import { cn } from "@/lib/utils"
+
+const functionMetadata = [
+    ["input(text: string)", "Opens a modal asking for a string and returns it."],
+    ["print(x: any)", "Prints an object."],
+    ["chart(data: Array<number>)", "Charts an array of numbers."],
+    ["login()", "Asks email and password and logs the Jutge client in."],
+]
 
 export default function PlaygroundHelp() {
     return (
-        <div>
-            <p className="text-xs pb-1.5 text-muted-foreground">HELP</p>
+        <TextWidth>
+            <p className="text-xs mb-2">HELP</p>
             <div
                 className={cn(
-                    "max-w-none text-sm px-2 py-1.5 border-spacing-2 rounded-lg bg-muted",
+                    "max-w-none text-sm px-4 py-3.5 rounded-md bg-muted",
                     "prose dark:prose-invert prose-code:before:hidden prose-code:after:hidden",
                 )}
             >
-                <ul>
+                <ul className="m-0 flex flex-col gap-1 *:m-0 *:pl-0 pl-4">
                     <li>
-                        Use <PlatformCtrlKbd keyName="Enter" className="relative bottom-[0.06rem] mx-1" /> to run the code of the last cell.
-                    </li>
-                    <li>
-                        Use <code>return x</code> to show <code>x</code> in the "OUTPUT" area. <code>last</code> contains the previous <code>return</code>ed value.
+                        Use{" "}
+                        <PlatformCtrlKbd
+                            keyName="Enter"
+                            className="relative bottom-[0.06rem] mx-1 bg-accent text-white"
+                        />{" "}
+                        to run the code in a cell.
                     </li>
                     <li>
                         Access the Jutge client through <code>jutge</code>, which is pre-initialized
                         with a <code>new JutgeApiClient()</code> object.
                     </li>
                     <li>
-                        Available functions, all <em>asynchronous</em>:
-                        <ul>
-                            <li >
-                                <code>input(text: string)</code>: opens a modal (showing <code>text</code>) asking for a string and returns
-                                it. 
-                            </li>
-                            <li>
-                                <code>print(x: any)</code>: prints an object (do not use{" "}
-                                <code>console.log</code>!).
-                            </li>
-                            <li>
-                                <code>chart(data: Array&lt;number&gt;)</code>: charts an array of numbers (<code>data</code>).
-                            </li>
-                            <li>
-                                <code>login()</code>: asks email and password and
-                                logs the <code>jutge</code> Jutge client in.
-                            </li>
-                        </ul>
+                        Use &quot;<code>return x</code>&quot; to show <code>x</code> in the OUTPUT
+                        area. &quot;<code>last</code>&quot; contains the previous{" "}
+                        <code>return</code>ed value.
                     </li>
                 </ul>
+                <p className="mt-4 mb-2">
+                    Available functions, all <em>asynchronous</em>:
+                </p>
+                <table className="m-0 border-t border-b border-black border-opacity-30">
+                    <tbody>
+                        {functionMetadata.map(([name, description]) => (
+                            <tr
+                                key={name}
+                                className="border-b border-black border-opacity-30 h-min leading-3"
+                            >
+                                <td>
+                                    <code className="text-[.75rem]">{name}</code>
+                                </td>
+                                <td>{description}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
-        </div>
+        </TextWidth>
     )
 }
