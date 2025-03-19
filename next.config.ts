630a3e038640bd0afd1d134aa69f9dc9d368d9d1
Jordi Petit <jordi.petit@gmail.com>
2025-03-19 17:37:38 +0100

    Removed rewrite to "/api"

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,9 @@
-import env from "@/lib/env"
 import createMDX from "@next/mdx"
 import rehypeShiki from "@shikijs/rehype"
 import type { NextConfig } from "next"
 import remarkGfm from "remark-gfm"
 
 const nextConfig: NextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: "/api/:path*",
-                destination: `${env.JUTGE_API_URL}/api/:path*`,
-            },
-            {
-                source: "/packs/:path*",
-                destination: `${env.JUTGE_API_URL}/packs/:path*`,
-            },
-        ]
-    },
     pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
     logging: {
         fetches: {
